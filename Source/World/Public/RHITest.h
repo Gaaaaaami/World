823fe1aa96449981d77bcf1aa57e25b8a5b87107
c418 <3116885284@qq.com>
2026-08-10 18:37:54 +0800

    RHI 测试项目

---
+++ Source/World/Public/RHITest.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "RHITest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WORLD_API URHITest : public UObject
+{
+	GENERATED_BODY()
+	
+};
