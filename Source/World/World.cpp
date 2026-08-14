038104e8e4a8fe9f11b8231811ad900ed4a0e1d2
c418 <3116885284@qq.com>
2026-08-14 12:18:44 +0800

    Shader路径映射改为全局方法

--- Source/World/World.cpp
+++ Source/World/World.cpp
@@ -4,29 +4,36 @@
 #include "Modules/ModuleManager.h"
 
 
+
+class ShaderInitialize {
+public:
+    ShaderInitialize(bool InAutoCirle = false)
+    {
+        // 👇 核心：把项目根目录的 "Shader" 文件夹映射为虚拟路径 "/Project"
+        FString ShaderDirectory = FPaths::Combine(FPaths::ProjectDir(), TEXT("Private"));
+        // ✅ 关键：映射前先判重，避免热重载崩溃
+        if (!AllShaderSourceDirectoryMappings().Contains(TEXT("/Project")))
+        {
+            AddShaderSourceDirectoryMapping(TEXT("/Project"), ShaderDirectory);
+        }
+
+        if (InAutoCirle)
+        {
+            delete this;
+        }
+    }
+
+};
+static ShaderInitialize ShaderInitializeInstance;
+
 void FWorldModule::StartupModule()
 {
-
     FDefaultGameModuleImpl::StartupModule();
-
-#if 1
-    // 👇 核心：把项目根目录的 "Shader" 文件夹映射为虚拟路径 "/Project"
-    FString ShaderDirectory = FPaths::Combine(FPaths::ProjectDir(), TEXT("Private"));
-    // ✅ 关键：映射前先判重，避免热重载崩溃
-    if (!AllShaderSourceDirectoryMappings().Contains(TEXT("/Project")))
-    {
-        AddShaderSourceDirectoryMapping(TEXT("/Project"), ShaderDirectory);
-        UE_LOG(LogTemp, Log, TEXT("[World] Shader directory mapped: %s"), *ShaderDirectory);
-    }
-    else
-    {
-        UE_LOG(LogTemp, Warning, TEXT("[World] /Project already mapped, skip"));
-    }
-#endif
 }
 
 void FWorldModule::ShutdownModule()
 {
     FDefaultGameModuleImpl::ShutdownModule();
 }
+
 IMPLEMENT_PRIMARY_GAME_MODULE(FWorldModule, World, "World");
